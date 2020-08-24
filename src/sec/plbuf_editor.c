40c992f885dd98d2e5dc28be755f4c0d4f1a776a
rafael-santiago <voidbrainvoid@tutanota.com>
2020-08-23 16:44:51 -0800

    Sec tests finished up

--- src/sec/plbuf_editor.c
+++ src/sec/plbuf_editor.c
@@ -6,6 +6,75 @@ static const kryptos_u8_t *findalias(const kryptos_u8_t *haystack, const kryptos
 
 static kryptos_u8_t *random_plbuf_entry();
 
+kryptos_u8_t *plbuf_edit_passwd(const kryptos_u8_t *plbuf, const size_t plbuf_size,
+                                const kryptos_u8_t *alias, const size_t alias_size,
+                                size_t *passwd_size) {
+    kryptos_task_ctx t, *ktask = &t;
+    kryptos_u8_t *passwd = NULL;
+    const kryptos_u8_t *p = NULL, *p_end = NULL, *end = NULL;
+    kryptos_u8_t *buf = NULL;
+    size_t buf_size = 0;
+
+    kryptos_task_init_as_null(ktask);
+
+    if (plbuf == NULL || plbuf_size == 0 || alias == NULL || alias_size == 0 || passwd_size == NULL) {
+        goto plbuf_edit_passwd_epilogue;
+    }
+
+    end = plbuf + plbuf_size;
+
+    p = findalias(plbuf, end, alias, alias + alias_size);
+
+    if (p == NULL) {
+        goto plbuf_edit_passwd_epilogue;
+    }
+
+    while (p != end && *p != '\t') {
+        p++;
+    }
+
+    if (p == end) {
+        goto plbuf_edit_passwd_epilogue;
+    }
+
+    p += 1;
+
+    p_end = p;
+    while (p_end != end && *p_end != '\n') {
+        p_end++;
+    }
+
+    buf_size = p_end - p;
+    if ((buf = (kryptos_u8_t *) kryptos_newseg(buf_size + 1)) == NULL) {
+        goto plbuf_edit_passwd_epilogue;
+    }
+    memset(buf, 0, buf_size + 1);
+    memcpy(buf, p, buf_size);
+
+    kryptos_task_set_decode_action(ktask);
+    kryptos_run_encoder(base64, ktask, buf, buf_size);
+
+plbuf_edit_passwd_epilogue:
+
+    if (kryptos_last_task_succeed(ktask)) {
+        passwd = ktask->out;
+        *passwd_size = ktask->out_size;
+    }
+
+    if (buf != NULL) {
+        kryptos_freeseg(buf, buf_size);
+        buf_size = 0;
+    }
+
+    kryptos_task_init_as_null(ktask);
+
+    p = p_end = end = NULL;
+    buf = NULL;
+    buf_size = 0;
+
+    return passwd;
+}
+
 int plbuf_edit_detach(kryptos_u8_t **plbuf, size_t *plbuf_size) {
     int err = 1;
     kryptos_u8_t *p = NULL, *p_end = NULL;
@@ -29,11 +98,15 @@ int plbuf_edit_detach(kryptos_u8_t **plbuf, size_t *plbuf_size) {
 
     p += 1;
 
-    p_end -= (p_end[0] != '\n') + (p_end[-1] == '\n');
+    p_end -= 1;
 
-    while (p_end != p && *p_end == '\n') {
+    p_end -= (*p_end == '\n');
+
+    while (p_end != p && *p_end != '\n') {
         p_end--;
     }
+
+    p_end += (*p_end == '\n');
 
     temp_size = p_end - p;
     if ((temp = (kryptos_u8_t *) kryptos_newseg(temp_size + 1)) == NULL) {
@@ -113,14 +186,16 @@ int plbuf_edit_shuffle(kryptos_u8_t **plbuf, size_t *plbuf_size) {
 
     p = (*plbuf);
 
+    plbuf_lines[plbuf_n++] = p++;
+
     while (p != p_end) {
-        if (*p == '\n') {
-            plbuf_lines[plbuf_n++] = p;
+        if (*p == '\n' && p + 1 != p_end) {
+            plbuf_lines[plbuf_n++] = p + 1;
         }
         p++;
     }
 
-    temp_size = *plbuf_size + random_entries_size[0] + random_entries_size[1] + 1;
+    temp_size = *plbuf_size + random_entries_size[0] + random_entries_size[1];
     temp = (kryptos_u8_t *) kryptos_newseg(temp_size + 1);
     memset(temp, 0, temp_size + 1);
 
@@ -129,10 +204,10 @@ int plbuf_edit_shuffle(kryptos_u8_t **plbuf, size_t *plbuf_size) {
     done = 0;
     plbuf_n = 0;
 
-#define random_plbuf_line_n ( (size_t) kryptos_get_random_byte() << 24 |\
-                              (size_t) kryptos_get_random_byte() << 16 |\
-                              (size_t) kryptos_get_random_byte() <<  8 |\
-                              (size_t) kryptos_get_random_byte() )
+#define random_plbuf_line_n ( ( (size_t) kryptos_get_random_byte() << 24 |\
+                                (size_t) kryptos_get_random_byte() << 16 |\
+                                (size_t) kryptos_get_random_byte() <<  8 |\
+                                (size_t) kryptos_get_random_byte() ) % plbuf_lines_nr )
 
     while (!done) {
         do {
@@ -150,7 +225,6 @@ int plbuf_edit_shuffle(kryptos_u8_t **plbuf, size_t *plbuf_size) {
             goto plbuf_edit_shuffle_epilogue;
         }
         p_end += 1;
-
         memcpy(tp, p, p_end - p);
         tp += p_end - p;
 
@@ -173,6 +247,7 @@ plbuf_edit_shuffle_epilogue:
     if (temp != NULL && err == 0) {
         kryptos_freeseg(*plbuf, *plbuf_size);
         (*plbuf) = temp;
+        (*plbuf_size) = temp_size;
         temp = NULL;
         temp_size = 0;
     } else if (temp != NULL) {
@@ -182,7 +257,7 @@ plbuf_edit_shuffle_epilogue:
     }
 
     if (plbuf_lines != NULL) {
-        kryptos_freeseg(*plbuf_lines, sizeof(kryptos_u8_t *) * plbuf_lines_nr);
+        kryptos_freeseg(plbuf_lines, sizeof(kryptos_u8_t *) * plbuf_lines_nr);
         plbuf_lines_nr = 0;
     }
 
@@ -234,16 +309,22 @@ int plbuf_edit_add(kryptos_u8_t **plbuf, size_t *plbuf_size,
         goto plbuf_edit_add_epilogue;
     }
 
-    temp_size = *plbuf_size + alias_size + 4;
-    temp = (kryptos_u8_t *) kryptos_newseg(temp_size + ktask->out_size);
-    memset(temp, 0, temp_size);
+    temp_size = *plbuf_size + alias_size + ktask->out_size + 2;
+    if ((temp = (kryptos_u8_t *) kryptos_newseg(temp_size + 1)) == NULL) {
+        goto plbuf_edit_add_epilogue;
+    }
+    memset(temp, 0, temp_size + 1);
+
     if ((*plbuf) != NULL) {
         memcpy(temp, *plbuf, *plbuf_size);
+    } else {
+        *plbuf_size = 0;
     }
+
     memcpy(temp + *plbuf_size, alias, alias_size);
     *(temp + *plbuf_size + alias_size) = '\t';
     memcpy(temp + *plbuf_size + alias_size + 1, ktask->out, ktask->out_size);
-    *(temp + *plbuf_size + alias_size + ktask->out_size + 1) = '\n';
+    *(temp + *plbuf_size + alias_size + ktask->out_size  + 1) = '\n';
 
     err = 0;
 
@@ -275,7 +356,7 @@ int plbuf_edit_del(kryptos_u8_t **plbuf, size_t *plbuf_size,
     kryptos_u8_t *pl, *pl_end;
     size_t pl_size;
 
-    if (plbuf == NULL || plbuf_size == NULL || (*plbuf) != NULL || *plbuf_size == 0 || alias == NULL || alias_size == 0) {
+    if (plbuf == NULL || plbuf_size == NULL || (*plbuf) == NULL || *plbuf_size == 0 || alias == NULL || alias_size == 0) {
         goto plbuf_edit_del_epilogue;
     }
 
@@ -290,15 +371,13 @@ int plbuf_edit_del(kryptos_u8_t **plbuf, size_t *plbuf_size,
     entry_end += 1;
 
     temp_size = *plbuf_size - (entry_end - entry);
-    temp = (kryptos_u8_t *) kryptos_newseg(temp_size);
+    temp = (kryptos_u8_t *) kryptos_newseg(temp_size + 1);
 
     if (temp == NULL) {
         goto plbuf_edit_del_epilogue;
     }
 
-    memset(temp, 0, temp_size);
-
-    entry_end += 1;
+    memset(temp, 0, temp_size + 1);
 
     if (entry == (*plbuf)) {
         if (entry_end < (*plbuf) + *plbuf_size) {
@@ -315,8 +394,7 @@ int plbuf_edit_del(kryptos_u8_t **plbuf, size_t *plbuf_size,
         pl_size = pl_end - pl;
         memcpy(temp, pl, pl_size);
         if (entry_end < (*plbuf) + *plbuf_size) {
-            pl_size = *plbuf_size - pl_size - (entry_end - entry);
-            memcpy(temp + pl_size, entry_end, pl_size);
+            memcpy(temp + pl_size, entry_end, *plbuf_size - pl_size - (entry_end - entry));
         }
     }
 
@@ -344,6 +422,9 @@ plbuf_edit_del_epilogue:
 
 int plbuf_edit_find(const kryptos_u8_t *plbuf, const size_t plbuf_size,
                     const kryptos_u8_t *alias, const size_t alias_size) {
+    if (alias_size == 0 || plbuf_size == 0) {
+        return 0;
+    }
     return (findalias(plbuf, plbuf + plbuf_size, alias, alias + alias_size) != NULL);
 }
 
@@ -389,14 +470,14 @@ static kryptos_u8_t *random_plbuf_entry(size_t *size) {
     memset(entry, 0, *size + 2);
 
     ep = entry;
-    ep_end = ep - *size - 1;
+    ep_end = ep + *size - 1;
 
     while (ep != ep_end) {
         *ep = randcharset[kryptos_get_random_byte() % randcharset_size];
         ep++;
     }
 
-    ep_end[1] = '\n';
+    ep_end[0] = '\n';
 
     return entry;
 }
--- src/sec/test/main.c
+++ src/sec/test/main.c
@@ -1,15 +1,154 @@
 #include <cutest.h>
 #include <sec/crypto.h>
+#include <sec/plbuf_editor.h>
 #include <ctx/ctx.h>
 #include <string.h>
 
 CUTE_DECLARE_TEST_CASE(sec_tests);
 CUTE_DECLARE_TEST_CASE(crypto_tests);
+CUTE_DECLARE_TEST_CASE(plbuf_editor_tests);
 
 CUTE_MAIN(sec_tests);
 
 CUTE_TEST_CASE(sec_tests)
     CUTE_RUN_TEST(crypto_tests);
+    CUTE_RUN_TEST(plbuf_editor_tests);
+CUTE_TEST_CASE_END
+
+CUTE_TEST_CASE(plbuf_editor_tests)
+    kryptos_u8_t *plbuf = NULL;
+    size_t plbuf_size = 0;
+    struct test_ctx {
+        kryptos_u8_t *alias;
+        kryptos_u8_t *passwd;
+    } test_vector[] = {
+        { "nasa", "12344321" },
+        { "cern", "0000000." },
+        { "home", "101010-----12939123" },
+    }, *test, *test_end;
+    kryptos_u8_t *passwd;
+    size_t passwd_size;
+
+    test = &test_vector[0];
+    test_end = test + sizeof(test_vector) / sizeof(test_vector[0]);
+
+    CUTE_ASSERT(plbuf_edit_add(NULL, &plbuf_size, test->alias, strlen(test->alias),
+                                   test->passwd, strlen(test->passwd)) != 0);
+    CUTE_ASSERT(plbuf_edit_add(&plbuf, NULL, test->alias, strlen(test->alias),
+                                   test->passwd, strlen(test->passwd)) != 0);
+    CUTE_ASSERT(plbuf_edit_add(&plbuf, &plbuf_size, NULL, strlen(test->alias),
+                                   test->passwd, strlen(test->passwd)) != 0);
+    CUTE_ASSERT(plbuf_edit_add(&plbuf, &plbuf_size, test->alias, 0,
+                                   test->passwd, strlen(test->passwd)) != 0);
+    CUTE_ASSERT(plbuf_edit_add(&plbuf, &plbuf_size, test->alias, strlen(test->alias),
+                                   NULL, strlen(test->passwd)) != 0);
+    CUTE_ASSERT(plbuf_edit_add(&plbuf, &plbuf_size, test->alias, strlen(test->alias),
+                                   test->passwd, 0) != 0);
+
+    while (test != test_end) {
+        CUTE_ASSERT(plbuf_edit_add(&plbuf, &plbuf_size, test->alias, strlen(test->alias),
+                                   test->passwd, strlen(test->passwd)) == 0);
+        test++;
+    }
+
+    CUTE_ASSERT(plbuf_edit_find(NULL, plbuf_size, "cern", 3) == 0);
+    CUTE_ASSERT(plbuf_edit_find(plbuf, 0, "cern", 3) == 0);
+    CUTE_ASSERT(plbuf_edit_find(plbuf, plbuf_size, NULL, 3) == 0);
+    CUTE_ASSERT(plbuf_edit_find(plbuf, plbuf_size, "cern", 0) == 0);
+
+    CUTE_ASSERT(plbuf_edit_find(plbuf, plbuf_size, "404", 3) == 0);
+
+    test = &test_vector[0];
+
+    while (test != test_end) {
+        CUTE_ASSERT(plbuf_edit_find(plbuf, plbuf_size, test->alias, strlen(test->alias)) == 1);
+        test++;
+    }
+
+    CUTE_ASSERT(plbuf_edit_del(&plbuf, &plbuf_size, "404", 3) != 0);
+
+    test = &test_vector[0];
+
+    CUTE_ASSERT(plbuf_edit_del(NULL, &plbuf_size, test->alias, strlen(test->alias)) != 0);
+    CUTE_ASSERT(plbuf_edit_del(&plbuf, 0, test->alias, strlen(test->alias)) != 0);
+    CUTE_ASSERT(plbuf_edit_del(&plbuf, &plbuf_size, NULL, strlen(test->alias)) != 0);
+    CUTE_ASSERT(plbuf_edit_del(&plbuf, &plbuf_size, test->alias, 0) != 0);
+
+    while (test != test_end) {
+        CUTE_ASSERT(plbuf_edit_del(&plbuf, &plbuf_size, test->alias, strlen(test->alias)) == 0);
+        test++;
+    }
+
+    test = &test_vector[0];
+
+    while (test != test_end) {
+        CUTE_ASSERT(plbuf_edit_add(&plbuf, &plbuf_size, test->alias, strlen(test->alias),
+                                   test->passwd, strlen(test->passwd)) == 0);
+        test++;
+    }
+
+    test = test_end - 1;
+
+    while (test != (&test_vector[0] - 1)) {
+        CUTE_ASSERT(plbuf_edit_del(&plbuf, &plbuf_size, test->alias, strlen(test->alias)) == 0);
+        test--;
+    }
+
+    test = &test_vector[0];
+
+    while (test != test_end) {
+        CUTE_ASSERT(plbuf_edit_add(&plbuf, &plbuf_size, test->alias, strlen(test->alias),
+                                   test->passwd, strlen(test->passwd)) == 0);
+        test++;
+    }
+
+    CUTE_ASSERT(plbuf_edit_del(&plbuf, &plbuf_size, "cern", strlen("cern")) == 0);
+    CUTE_ASSERT(plbuf_edit_del(&plbuf, &plbuf_size, "home", strlen("home")) == 0);
+    CUTE_ASSERT(plbuf_edit_del(&plbuf, &plbuf_size, "nasa", strlen("nasa")) == 0);
+
+    test = &test_vector[0];
+
+    while (test != test_end) {
+        CUTE_ASSERT(plbuf_edit_add(&plbuf, &plbuf_size, test->alias, strlen(test->alias),
+                                   test->passwd, strlen(test->passwd)) == 0);
+        test++;
+    }
+
+    CUTE_ASSERT(plbuf_edit_del(&plbuf, &plbuf_size, "cern", strlen("cern")) == 0);
+    CUTE_ASSERT(plbuf_edit_del(&plbuf, &plbuf_size, "nasa", strlen("nasa")) == 0);
+    CUTE_ASSERT(plbuf_edit_del(&plbuf, &plbuf_size, "home", strlen("home")) == 0);
+
+    test = &test_vector[0];
+
+    while (test != test_end) {
+        CUTE_ASSERT(plbuf_edit_add(&plbuf, &plbuf_size, test->alias, strlen(test->alias),
+                                   test->passwd, strlen(test->passwd)) == 0);
+        test++;
+    }
+
+    CUTE_ASSERT(plbuf_edit_shuffle(&plbuf, &plbuf_size) == 0);
+    CUTE_ASSERT(plbuf_edit_detach(&plbuf, &plbuf_size) == 0);
+
+    test = &test_vector[0];
+
+    CUTE_ASSERT(plbuf_edit_passwd(NULL, plbuf_size, test->alias, strlen(test->alias), &passwd_size) == NULL);
+    CUTE_ASSERT(plbuf_edit_passwd(plbuf, 0, test->alias, strlen(test->alias), &passwd_size) == NULL);
+    CUTE_ASSERT(plbuf_edit_passwd(plbuf, plbuf_size, NULL, strlen(test->alias), &passwd_size) == NULL);
+    CUTE_ASSERT(plbuf_edit_passwd(plbuf, plbuf_size, test->alias, 0, &passwd_size) == NULL);
+    CUTE_ASSERT(plbuf_edit_passwd(plbuf, plbuf_size, test->alias, strlen(test->alias), NULL) == NULL);
+
+    while (test != test_end) {
+        passwd = plbuf_edit_passwd(plbuf, plbuf_size, test->alias, strlen(test->alias), &passwd_size);
+        CUTE_ASSERT(passwd != NULL);
+        CUTE_ASSERT(passwd_size == strlen(test->passwd));
+        CUTE_ASSERT(memcmp(passwd, test->passwd, passwd_size) == 0);
+        kryptos_freeseg(passwd, passwd_size);
+        test++;
+    }
+
+    CUTE_ASSERT(plbuf_edit_passwd(plbuf, plbuf_size, "Greenfuzz", strlen("Greenfuzz"), &passwd_size) == NULL);
+
+    kryptos_freeseg(plbuf, plbuf_size);
 CUTE_TEST_CASE_END
 
 CUTE_TEST_CASE(crypto_tests)
