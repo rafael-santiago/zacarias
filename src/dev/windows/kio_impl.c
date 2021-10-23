0d35dded6fdde97e066c9c44b70da9b2323d2465
rafael-santiago <voidbrainvoid@tutanota.com>
2021-10-23 20:59:31 -0300

    Implement kread_impl for Windows driver

--- src/dev/windows/kio_impl.c
+++ src/dev/windows/kio_impl.c
@@ -7,24 +7,25 @@
  */
 #include <windows/kio_impl.h>
 #include <ntifs.h>
+#include <kryptos.h>
 
 int kwrite_impl(const char *filepath, void *buf, const size_t buf_size) {
-    if (KeGetCurrentIrql() != PASSIVE_LEVEL) {
-        return -1;
-    }
-
     OBJECT_ATTRIBUTES file_attr;
     HANDLE file_handle;
     UNICODE_STRING filepath_ustr;
     ANSI_STRING temp_astr;
     NTSTATUS status = STATUS_UNSUCCESSFUL;
     IO_STATUS_BLOCK io_sts;
-    int retval = -1;
+    int retval = 1;
+
+    if (KeGetCurrentIrql() != PASSIVE_LEVEL) {
+        return 1;
+    }
 
     RtlInitAnsiString(&temp_astr, filepath);
     status = RtlAnsiStringToUnicodeString(&filepath_ustr, &temp_astr, FALSE);
     if (!NT_SUCCESS(status)) {
-        return -1;
+        return 1;
     }
 
     InitializeObjectAttributes(&file_attr, &filepath_ustr, OBJ_KERNEL_HANDLE, NULL, NULL);
@@ -53,8 +54,71 @@ int kwrite_impl(const char *filepath, void *buf, const size_t buf_size) {
 }
 
 int kread_impl(const char *filepath, void **buf, size_t *buf_size) {
-    UNREFERENCED_PARAMETER(filepath);
-    UNREFERENCED_PARAMETER(buf);
-    UNREFERENCED_PARAMETER(buf_size);
+    OBJECT_ATTRIBUTES file_attr;
+    HANDLE file_handle = 0;
+    UNICODE_STRING filepath_ustr;
+    ANSI_STRING temp_astr;
+    NTSTATUS status = STATUS_UNSUCCESSFUL, create_file_status = STATUS_UNSUCCESSFUL;
+    IO_STATUS_BLOCK io_sts;
+    int retval = 1;
+    FILE_STANDARD_INFORMATION file_info = { 0 };
+
+    if (KeGetCurrentIrql() != PASSIVE_LEVEL || filepath == NULL || buf == NULL || buf_size == NULL) {
+        return 1;
+    }
+
+    *buf_size = 0;
+
+    RtlInitAnsiString(&temp_astr, filepath);
+    status = RtlAnsiStringToUnicodeString(&filepath_ustr, &temp_astr, FALSE);
+    if (!NT_SUCCESS(status)) {
+        goto kread_impl_epilogue;
+    }
+
+    InitializeObjectAttributes(&file_attr, &filepath_ustr, OBJ_KERNEL_HANDLE, NULL, NULL);
+    create_file_status = ZwCreateFile(&file_handle,
+                                      GENERIC_READ,
+                                      &file_attr,
+                                      &io_sts,
+                                      NULL,
+                                      FILE_ATTRIBUTE_NORMAL,
+                                      0,
+                                      FILE_OPEN,
+                                      FILE_SYNCHRONOUS_IO_NONALERT,
+                                      NULL,
+                                      0);
+    if (NT_SUCCESS(status)) {
+        status = ZwQueryInformationFile(file_handle,
+                                        &io_sts,
+                                        &file_info,
+                                        sizeof(file_info),
+                                        FileStandardInformation);
+
+        if (!NT_SUCCESS(status)) {
+            goto kread_impl_epilogue;
+        }
+
+        *buf_size = (size_t) file_info.EndOfFile.QuadPart;
+
+        *buf = kryptos_newseg(*buf_size + 1);
+        if (*buf == NULL) {
+            *buf_size = 0;
+            goto kread_impl_epilogue;
+        }
+
+        status = ZwReadFile(file_handle, NULL, NULL, NULL, &io_sts, *buf, (ULONG)*buf_size, NULL, NULL);
+        if (NT_SUCCESS(status)) {
+            retval = (int)*buf_size;
+        } else {
+            kryptos_freeseg(*buf, *buf_size);
+            *buf_size = 0;
+        }
+    }
+
+kread_impl_epilogue:
+    if (NT_SUCCESS(create_file_status)) {
+        ZwClose(file_handle);
+    }
+
     return 1;
 }
